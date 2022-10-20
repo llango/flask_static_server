93c074c5262f40c6677fe3c55e84468c3b146ff0
llango <724261350@qq.com>
2022-10-20 10:59:43 +0800

    修改为docs

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from whitenoise import WhiteNoise
 
 app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='docs/')
 
 
 if __name__ == "__main__":
